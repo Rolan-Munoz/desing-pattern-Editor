933634c678ead6b621c4b79437502e5f3d924998
Rolan Muñoz <rolanmunoz.dev@gmail.com>
2023-04-16 11:47:29 +0200

    create class CareTaker

---
+++ editor/src/main/java/com/kreitek/editor/CareTaker.java
@@ -0,0 +1,18 @@
+package com.kreitek.editor;
+
+import java.util.Stack;
+
+public class CareTaker {
+    private Stack<Memento> mementos = new Stack<>();
+
+    public void addMemento(Memento memento) {
+        mementos.push(memento);
+    }
+
+    public Memento restore() {
+        if (mementos.isEmpty()) {
+            return null;
+        }
+        return mementos.pop();
+    }
+}
